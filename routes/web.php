14587548504c1671ec8ed2d680374798fcab0534
adedayojs <samfeolu@gmail.com>
2023-08-07 09:19:43 +0100

    update route

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::post('/meal-request', [MealRequestController::class, 'store'])->name('meal-request.create');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
